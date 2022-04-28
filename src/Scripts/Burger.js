080f7ad8d6d87bf6fd12243d9001291c0ec51c2d
Jurv1 <bushmanhik931@mail.ru>
2022-04-28 05:41:53 +0300

    refactoring

--- src/Scripts/Burger.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    const burger = document.querySelector('.burger');
-    const menuBody = document.querySelector('.menu__body');
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('burger__active');
-        menuBody.classList.toggle('_active');
-    });
-    
-}());
